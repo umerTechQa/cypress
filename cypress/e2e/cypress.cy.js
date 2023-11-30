f5b3d606ea473e8634ecb9be49db74f012125ebc
umerTechQa <109993016+umerTechQa@users.noreply.github.com>
2023-11-30 13:50:10 +0500

    first commit

---
+++ cypress/e2e/cypress.cy.js
@@ -0,0 +1,12 @@
+describe('mytest',()=>{
+  it('Does work ',()=>{
+  expect(true).to.equal(true)
+  
+  cy.visit('https://example.cypress.io/commands/actions')
+  cy.get('#email1').type('hello@gmail.com').should('have.value','hello@gmail.com')
+  cy.get('#password1').type('12345678').focus().should('have.value','12345678').prev().should('have.attr','style','color: orange;')
+  cy.get('#fullName1').type('Enter First Name').blur().should('have.value','Enter First Name').prev().should('have.attr','style','color: red;')
+  cy.get('#description').type('Clear text').should('have.value','Clear text').clear().should('have.value', '')
+})
+console.log("Done!!!!");
+})
